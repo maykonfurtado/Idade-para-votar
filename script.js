f8449b34546a0b5eb451ce80a2c5c3fabc8d5a57
MAYKON FURTADO <86491299+maykonfurtado@users.noreply.github.com>
2024-04-29 16:30:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+document.getElementById('ageForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita o envio padrão do formulário
 
-const idade = parseInt(prompt("Digite sua idade:"));
-if (idade < 16) {
-  console.log("Você ainda não tem idade para votar.");
-} else if (idade >= 16 && idade <= 17) {
-  console.log("Seu voto é opcional.");
-} else if (idade >= 18 && idade <= 70) {
-  console.log("Seu voto é obrigatório.");
-} else {
-  console.log("Seu voto é opcional.");
-}
+  const idade = parseInt(document.getElementById('age').value);
+  let mensagem;
+
+  if (idade < 16) {
+      mensagem = "Você ainda não tem idade para votar.";
+  } else if (idade >= 16 && idade <= 17) {
+      mensagem = "Seu voto é opcional.";
+  } else if (idade >= 18 && idade <= 70) {
+      mensagem = "Seu voto é obrigatório.";
+  } else {
+      mensagem = "Seu voto é opcional.";
+  }
+
+  document.getElementById('result').innerText = mensagem;
+});
